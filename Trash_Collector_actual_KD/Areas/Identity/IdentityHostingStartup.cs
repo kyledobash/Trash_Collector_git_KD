dd29a0191220c85e498f64559f191bf1417916b4
kyledobash <kyledobash@yahoo.com>
2021-01-29 12:39:12 -0700

    init (Adding Foreign Key reference step)

---
+++ Trash_Collector_actual_KD/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trash_Collector_actual_KD.Data;
+
+[assembly: HostingStartup(typeof(Trash_Collector_actual_KD.Areas.Identity.IdentityHostingStartup))]
+namespace Trash_Collector_actual_KD.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Trash_Collector_actual_KD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trash_Collector_actual_KD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

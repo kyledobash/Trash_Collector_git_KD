95b763965bb28e17fe3c09e2336eb59de46182ab
kyledobash <kyledobash@yahoo.com>
2021-02-01 08:44:57 -0700

    built the customer model

---
+++ Trash_Collector_actual_KD/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector_actual_KD.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string State { get; set; }
+
+        public double Balance { get; set; }
+
+        public string WeeklyPickupDay { get; set; }
+
+        public DateTime OneTimePickup { get; set; }
+
+        public DateTime StartSuspendService { get; set; }
+
+        public DateTime EndSuspendService { get; set; }
+    }
+}
---
+++ Trash_Collector_actual_KD/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector_actual_KD.Models
+{
+    public class Employee
+    {
+    }
+}

844ea2580f15d5cce2d936c04489d92c7c7a38bb
kyledobash <kyledobash@yahoo.com>
2021-02-01 08:47:54 -0700

    built the Employee model.

--- Trash_Collector_actual_KD/Models/Employee.cs
+++ Trash_Collector_actual_KD/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace Trash_Collector_actual_KD.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int ServiceAreaZipCode { get; set; }
+
+        public List<Customer> PendingPickups { get; set; }
+
+        public List<Customer> CompletedPickups { get; set; }
     }
 }
